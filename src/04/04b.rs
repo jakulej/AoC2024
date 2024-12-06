b3a6157d5ff037bc83094783c642bbfd044af6c0
jakulej <j.kulej01@gmail.com>
2024-12-06 12:21:59 +0100

    4b working

--- src/04/04b.rs
+++ src/04/04b.rs
@@ -1,21 +1,77 @@
+const PATTERN_SKIP_CHAR: u8 = b'.';
+const PATTERN_MIDDLE_CHAR: u8 = b'A';
+const PATTERNS: [[&str;3];4] = [
+    [
+        "M.S",
+        ".A.",
+        "M.S",
+    ],
+    [
+        "M.M",
+        ".A.",
+        "S.S",
+    ],
+    [
+        "S.M",
+        ".A.",
+        "S.M",
+    ],
+    [
+        "S.S",
+        ".A.",
+        "M.M",
+    ]
+    
+];
+
+
 fn main() {
-    let pattern_skip_char = b'.';
-    let pattern = 
+    let input_file: Vec<Vec<u8>> = include_bytes!("input.txt")
+        .split(|b| b == &b'\n')
+        .map(|line| line.to_vec())
+        .collect();
+    let score = input_file
+        .iter()
+        .enumerate()
+        .map(|(y, line)| {
+            line.iter()
+                .enumerate()
+                .filter(|(_x, char)| char == &&PATTERN_MIDDLE_CHAR)
+                .map(|(x, _char)| {
 
+                    if PATTERNS.iter().any(|pattern| is_pattern_matching(input_file.clone(), pattern.clone(), (x, y))){
+                        1
+                    }else{
+                        0
+                    }
 
-    let input_file: Vec<Vec<u8>> = include_bytes!("example.txt")
-    .split(|b| b == &b'\n')
-    .map(|line| line.to_vec())
-    .collect();
-
-    let x = input_file[2].windows(3).filter(|window| window[1] == b'X').count();
-    println!("{:?}",x);
-
+                })
+                .sum::<usize>()
+        })
+        .sum::<usize>();
+    print!("{:?}",score);
 }
+fn is_pattern_matching(
+    file: Vec<Vec<u8>>,
+    pattern: [&str;3],
+    coordinate: (usize, usize),
+) -> bool {
+ (0..pattern.len()).all(|y_pattern| {
+        (0..pattern.len()).all(|x_pattern| {
+            
+            let x_to_check = (coordinate.0 as isize - 1isize) as usize + x_pattern ;
+            let y_to_check = (coordinate.1 as isize - 1isize) as usize + y_pattern;
 
-fn file_to_2d_vec ( file_name: String) -> Vec<Vec<u8>> {
-    include_bytes!(file_name)
-    .split(|b| b == &b'\n')
-    .map(|line| line.to_vec())
-    .collect();
+            let is_xy_not_correct = x_to_check >= file[0].len() - 1 || y_to_check >= file.len();
+
+            if is_xy_not_correct{
+                return false;
+            }
+            
+            pattern[y_pattern].as_bytes()[x_pattern] == PATTERN_SKIP_CHAR ||
+            pattern[y_pattern].as_bytes()[x_pattern] == file[y_to_check][x_to_check]
+        })
+    })
+
+
 }
