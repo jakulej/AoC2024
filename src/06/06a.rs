b052d37272aa52eec516e2392a4f300759ab36b1
jakulej <jkul@thaumatec.com>
2024-12-10 12:53:10 +0100

    Create enum for 6a

--- src/06/06a.rs
+++ src/06/06a.rs
@@ -1,3 +1,41 @@
+const OBSTACLE: u8 = b'#';
+const GUARD: u8 = b'^';
+enum GuardFacing {
+    Up ,
+    Down,
+    Left,
+    Right
+}
+impl GuardFacing {
+    pub fn get_direction_vec(&self) -> (isize, isize) {
+        match self {
+            GuardFacing::Up => (0,1),
+            GuardFacing::Down => (0,-1),
+            GuardFacing::Left => (-1,0),
+            GuardFacing::Right => (1,0)
+        }
+    }
+}
+
+
 fn main () {
-    
+    let map: Vec<Vec<u8>> = include_bytes!("example.txt")
+        .split(|b| b == &b'\n')
+        .map(|line| line.to_vec())
+        .collect();
+    let guard_coords = find_guard(map).unwrap();
+    let guard_facing:GuardFacing = GuardFacing::Up; 
+
+
+}
+
+fn find_guard(map: Vec<Vec<u8>>) -> Option<(usize,usize)> {
+    for x in 0..map[0].len() {
+       for y in 0..map.len() {
+           if map[y][x] == GUARD {
+            return Some((x,y));
+           }
+       } 
+    }
+    None
 }
