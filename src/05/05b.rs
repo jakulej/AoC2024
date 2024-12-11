8937d0d40fd1b16eece9b156e38f4bc391089fba
jakulej <j.kulej01@gmail.com>
2024-12-11 18:46:40 +0100

    task 5b done

--- src/05/05b.rs
+++ src/05/05b.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
 fn main() {
-    let (ordering_rules, pages_to_produce) = include_str!("input.txt").split_once("\n\n").unwrap();
+    let (ordering_rules, pages_to_produce) = include_str!("input.txt").split_once("\r\n\r\n").unwrap();
 
     let mut number_orders: HashMap<usize, Vec<usize>> = HashMap::new();
-    ordering_rules.split("\n").for_each(|line| {
+    ordering_rules.split("\r\n").for_each(|line| {
         let (first_digit, second_digit) = line.split_once("|").unwrap_or(("0", "0"));
         number_orders
             .entry(first_digit.parse::<usize>().unwrap())
@@ -17,15 +17,22 @@ fn main() {
     });
 
     let score = pages_to_produce
-        .split("\n")
+        .split("\r\n")
         .filter_map(|line| {
             let updates: Vec<usize> = line
                 .split(",")
                 .map(|update| update.parse::<usize>().unwrap())
                 .collect();
 
-            match is_sorted(&updates, &number_orders) {
-                true => Some(get_middle_element(&updates)),
+            match !is_sorted(&updates, &number_orders) {
+                true => {
+                    print!("\n");
+                    print!("Not sorted: {:?}, ",updates);
+                    let sorted = &sort_orders(number_orders.clone(), updates);
+                    let middle_elemment = get_middle_element(sorted);
+                    println!("after sort: {:?}, middle element: {:?}",sorted,middle_elemment);
+                    Some(middle_elemment)
+                },
                 false => None,
             }
         })
@@ -49,29 +56,27 @@ fn get_middle_element(updates: &Vec<usize>) -> usize {
 }
 
 //NOT NEEDED
-fn sort_orders(number_orders: HashMap<usize, Vec<usize>>) -> Vec<usize> {
-    let mut sorted_vaules: Vec<usize> = Vec::new();
-    println!("{:?}", number_orders.keys());
-    while sorted_vaules.len() != number_orders.len() {
-        let key_wtih_no_occurence = number_orders
-            .keys()
-            .filter(|key| !sorted_vaules.contains(key))
-            .find(|next_key_to_append| {
-                number_orders
-                    .iter()
-                    .filter(|(key, _value)| !sorted_vaules.contains(key))
-                    .all(|(_key, value)| value.contains(next_key_to_append))
-            });
-        println!("{:?}", key_wtih_no_occurence);
-        sorted_vaules.push(*key_wtih_no_occurence.unwrap());
+fn sort_orders(number_orders: HashMap<usize, Vec<usize>>, current_updates: Vec<usize>) -> Vec<usize> {
+    let mut sorted_vaules: Vec<usize> = current_updates.clone();
+
+    for i in 0..current_updates.len() {
+        for j in 0..i {
+            let update = sorted_vaules[i];
+            let previous_update = sorted_vaules[j];
+            let is_current_update_sorted = match  number_orders.get(&update) {
+                Some(x) => !x.contains(&previous_update),
+                None => true,
+            };
+
+            if !is_current_update_sorted {
+                println!("{:?} should be after {:?}",update,previous_update);
+                println!("Removing value: {:?}, i {:?}, pushing to: {:?}",update,i,j);
+                let deleted = sorted_vaules.remove(i);
+                sorted_vaules.insert(j, deleted);
+                println!("Actual table: {:?}",sorted_vaules);
+            }
+        }
     }
-    sorted_vaules.push(
-        *number_orders
-            .get(sorted_vaules.last().unwrap())
-            .unwrap()
-            .get(0)
-            .unwrap(),
-    );
     sorted_vaules
 }
 
